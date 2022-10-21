0ab21651f815ccd7cd6d31072c2cc6904434a6e4
Matthew Herber <32679887+Happyrobot33@users.noreply.github.com>
2022-10-21 11:29:52 -0400

    Update thing-existing.js

--- client/pages/filespage/thing-existing.js
+++ client/pages/filespage/thing-existing.js
@@ -571,12 +571,17 @@ class LazyLoadImage extends React.Component {
     }
 
     render() {
-        if ((memory.get(this.props.src) === null) || this.state.error === true) {
+        if ((this.props.preview || memory.get(this.props.src) === null) || this.state.error === true) {
+            const ext = path.extname(this.props.path).replace(/^\./, "");
             return (
-                <img
-                    ref={this.$el}
-                    className={this.props.className}
-                    src={this.props.src} />
+                <span>
+                    <Icon name={this.props.icon} />
+                    <NgIf
+                        className="info_extension"
+                        cond={!!ext && this.props.view === "grid" && this.props.icon === "file" && this.props.hide_extension !== true}>
+                        <span>{ext}</span>
+                    </NgIf>
+                </span>
             );
         }
         return (
